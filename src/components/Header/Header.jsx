4535aa1122770d652da93075d097e24788db8049
Maxim Melikhov <melekhov25@gmail.com>
2024-05-06 00:22:04 +0300

    hooks5

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button } from "semantic-ui-react";
+import { Button } from "./Button";
 import { useTelegram } from "../../hooks/useTelegram";
 
 const Header = () => {
