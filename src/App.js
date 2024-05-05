898663bbbfe17836766372037354146faaf30840
Maxim Melikhov <melekhov25@gmail.com>
2024-05-06 00:05:31 +0300

    hooks3

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useEffect } from 'react';
 import './App.css';
 import { useTelegram } from './hooks/useTelegram';
+import { Header } from 'semantic-ui-react';
 
 function App() {
   const { onToogleButton, tg } = useTelegram()
@@ -11,6 +12,7 @@ function App() {
 
   return (
     <div className="App">
+      <Header />
       <button onClick={onToogleButton}>toggle</button>
     </div>
   );
