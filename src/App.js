29f4a9bed391a8b2c479666d6e088260aa8ffc77
Maxim Melikhov <melekhov25@gmail.com>
2024-05-06 00:32:21 +0300

    header2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { useEffect } from 'react';
 import './App.css';
 import { useTelegram } from './hooks/useTelegram';
-import { Header } from './components/Header/Header';
+import { Header } from 'src/components/Header/Header.jsx';
 
 function App() {
   const { onToogleButton, tg } = useTelegram()
